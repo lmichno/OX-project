692d0c49c6c00ec800dafaad2beace9fc7fbc860
LOLEKmilion <121362572+LOLEKmilion@users.noreply.github.com>
2024-04-18 10:22:17 +0200

    Add AJAX functionality to choose player

--- server.php
+++ server.php
@@ -1,3 +1,13 @@
 <?php
-session_start();
-print_r($_SESSION);
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+if (isset($_POST['player'])) {
+    $player = $_POST['player'];
+    if ($player === 'X') {
+        $nextPlayer = 'O';
+    } else {
+        $nextPlayer = 'X';
+    }
+    echo json_encode(array('player' => $player, 'nextPlayer' => $nextPlayer));
+}
