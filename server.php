2ce873194be41970fa4f4f0e99d4972e548b2ecb
LOLEKmilion <121362572+LOLEKmilion@users.noreply.github.com>
2024-04-21 14:39:06 +0200

    Update server.php

--- server.php
+++ server.php
@@ -1,13 +1,9 @@
 <?php
-
+session_start();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 if (isset($_POST['player'])) {
     $player = $_POST['player'];
-    if ($player === 'X') {
-        $nextPlayer = 'O';
-    } else {
-        $nextPlayer = 'X';
-    }
+
     echo json_encode(array('player' => $player, 'nextPlayer' => $nextPlayer));
 }
